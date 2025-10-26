a65d63fcc84f7f9dfe3df540e3cfcbbf6650b975
Myself <38400384+Myself-Bloons@users.noreply.github.com>
2025-10-26 19:29:34 +0100

    Update ModHelperData.cs
    Credits.

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,7 +2,7 @@ namespace AllSeeking;
 public static class ModHelperData
 {
     public const string Name = "All Seeking Projectiles";
-    public const string Description = "Makes all straight-traveling projectiles seek targets. Toggle with F9 hotkey or in-game mod settings.";
+    public const string Description = "Makes all straight-traveling projectiles seek targets. Toggle with F9 hotkey or in-game mod settings. Fix from an old Timotheeee mod.";
     public const string Version = "1.0.0";
     public const string RepoOwner = "Myself-Bloons";
     public const string RepoName = "AllSeekingProjectiles";
